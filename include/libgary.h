e8fcef69422ea3e1d540f0085da6d1299346806e
Kevin <kxkleiman@gmail.com>
2024-05-04 15:12:34 -0400

    add copyright

--- include/libgary.h
+++ include/libgary.h
@@ -1,3 +1,12 @@
+/* 
+ * Copyright (C) 2024 Kevin Kleiman - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * for any purpose
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: , or visit :
+ */
+
 #pragma once
 
 void gary();
--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
-#include <libgary.h>
+/* 
+ * Copyright (C) 2024 Kevin Kleiman - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * for any purpose
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: , or visit :
+ */
 
+#include <stdio.h>
+#include "libgary.h"
 
 int main()
 {
-    printf("Gary GameBoy emulator\n");
+    printf("gary init\n");
+
     gary();
+
     return 0;
 }
